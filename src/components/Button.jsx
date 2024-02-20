589337a8e794a49179706564ac8fcd9d020decb8
Jonathan Rheaume <emailme@jonathanrheaume.com>
2024-02-20 13:33:27 -0800

    Add Business Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
-export default function Button() {
+export default function Button({ styles }) {
   return (
-    <div>
-      <div>Button</div>
-    </div>
+    <button
+      type="button"
+      className={`bg-blue-gradient rounded-[10px] px-6 py-4 font-poppins text-[18px] font-medium text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
   );
 }
